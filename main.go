924a6d0324160e31096bd33429a7008206e94d2f
iyoussef <ibraheemyoussefiy@gmail.com>
2023-06-29 22:34:11 +0100

    Go-reload update, must review input and output

--- main.go
+++ main.go
@@ -3,197 +3,233 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
+	"testing"
 	"unicode"
 )
 
-// Hexadecimal to Decimal conversion
-func hexToDec(hex string) string {
-	dec := 0
-	base := 1
+// Configuration represents a modification configuration.
+type Configuration struct {
+	Search  string
+	Replace string
+	Number  int
+}
 
-	for i := len(hex) - 1; i >= 0; i-- {
-		if hex[i] >= '0' && hex[i] <= '9' {
-			dec += int(hex[i]-'0') * base
-		} else if hex[i] >= 'A' && hex[i] <= 'F' {
-			dec += int(hex[i]-'A'+10) * base
-		} else if hex[i] >= 'a' && hex[i] <= 'f' {
-			dec += int(hex[i]-'a'+10) * base
+var (
+	configurations = []Configuration{
+		{Search: "(hex)", Replace: "${dec}"},
+		{Search: "(bin)", Replace: "${dec}"},
+		{Search: "(up)", Replace: "${up}"},
+		{Search: "(low)", Replace: "${low}"},
+		{Search: "(cap)", Replace: "${cap}"},
+	}
+
+	punctuationRegex = regexp.MustCompile(`([.,!?;:])\s*`)
+	ellipsisRegex    = regexp.MustCompile(`\.{3}`)
+	exclamationRegex = regexp.MustCompile(`[!?]`)
+	quoteRegex       = regexp.MustCompile(`'([^']*)'`)
+	vowelRegex       = regexp.MustCompile(`(?i)\ba\s([aeiouh])`)
+)
+
+// ReplaceHex replaces hexadecimal numbers with their decimal equivalent.
+func ReplaceHex(text string) string {
+	hexRegex := regexp.MustCompile(`\b(0x[0-9A-Fa-f]+)\b`)
+	return hexRegex.ReplaceAllStringFunc(text, func(hex string) string {
+		dec, err := strconv.ParseInt(hex[2:], 16, 64)
+		if err != nil {
+			log.Println("Error converting hex to decimal:", err)
+			return hex
 		}
-		base *= 16
-	}
-
-	return fmt.Sprintf("%d", dec)
+		return strconv.FormatInt(dec, 10)
+	})
 }
 
-// Binary to Decimal conversion
-func binToDec(bin string) string {
-	dec := 0
-	base := 1
-
-	for i := len(bin) - 1; i >= 0; i-- {
-		if bin[i] == '1' {
-			dec += base
+// ReplaceBin replaces binary numbers with their decimal equivalent.
+func ReplaceBin(text string) string {
+	binRegex := regexp.MustCompile(`\b(0b[01]+)\b`)
+	return binRegex.ReplaceAllStringFunc(text, func(bin string) string {
+		dec, err := strconv.ParseInt(bin[2:], 2, 64)
+		if err != nil {
+			log.Println("Error converting bin to decimal:", err)
+			return bin
 		}
-		base *= 2
-	}
-
-	return fmt.Sprintf("%d", dec)
+		return strconv.FormatInt(dec, 10)
+	})
 }
 
-// Applies the requested modifications to the text
-func applyModifications(text string) string {
-	re := regexp.MustCompile(`\(([a-zA-Z]+)(?:,\s*(\d+))?\)`)
+// capitalizeWord capitalizes a word based on the specified case.
+func capitalizeWord(word string, caseType rune) string {
+	switch caseType {
+	case 'u':
+		return strings.ToUpper(word)
+	case 'l':
+		return strings.ToLower(word)
+	case 'c':
+		return strings.Title(strings.ToLower(word))
+	default:
+		return word
+	}
+}
 
-	words := strings.Fields(text)
-	modifiedWords := make([]string, 0, len(words))
-
-	for _, word := range words {
-		matches := re.FindStringSubmatch(word)
-
-		if len(matches) > 0 {
-			switch matches[1] {
-			case "hex":
-				modifiedWords = append(modifiedWords, hexToDec(matches[2]))
-			case "bin":
-				modifiedWords = append(modifiedWords, binToDec(matches[2]))
-			case "up":
-				if len(matches) == 3 {
-					n := min(len(modifiedWords), atoi(matches[2]))
-					modifiedWords = append(modifiedWords[:len(modifiedWords)-n], toUpper(modifiedWords[len(modifiedWords)-n:])...)
-				} else {
-					modifiedWords = append(modifiedWords, toUpper([]string{modifiedWords[len(modifiedWords)-1]})...)
-				}
-			case "low":
-				if len(matches) == 3 {
-					n := min(len(modifiedWords), atoi(matches[2]))
-					modifiedWords = append(modifiedWords[:len(modifiedWords)-n], toLower(modifiedWords[len(modifiedWords)-n:])...)
-				} else {
-					modifiedWords = append(modifiedWords, toLower([]string{modifiedWords[len(modifiedWords)-1]})...)
-				}
-			case "cap":
-				if len(matches) == 3 {
-					n := min(len(modifiedWords), atoi(matches[2]))
-					modifiedWords = append(modifiedWords[:len(modifiedWords)-n], capitalize(modifiedWords[len(modifiedWords)-n:])...)
-				} else {
-					modifiedWords = append(modifiedWords, capitalize([]string{modifiedWords[len(modifiedWords)-1]})...)
-				}
-			}
-		} else if strings.HasPrefix(word, "'") && strings.HasSuffix(word, "'") {
-			word = strings.Trim(word, "'")
-			modifiedWords = append(modifiedWords, "'"+word+"'")
-		} else if strings.HasSuffix(word, "...") || strings.HasSuffix(word, "!?") {
-			modifiedWords = append(modifiedWords, word)
-		} else if word == "a" && len(modifiedWords) < len(words)-1 && isVowelOrH(words[len(modifiedWords)+1]) {
-			modifiedWords = append(modifiedWords, "an")
-		} else {
-			modifiedWords = append(modifiedWords, word)
+// ReplaceCase replaces words based on case modification configurations.
+func ReplaceCase(text string) string {
+	return quoteRegex.ReplaceAllStringFunc(text, func(quote string) string {
+		quote = strings.Trim(quote, "'")
+		parts := strings.Split(quote, " ")
+		if len(parts) != 2 {
+			return quote
 		}
-	}
 
-	return strings.Join(modifiedWords, " ")
-}
-
-// Helper functions
-func toUpper(words []string) []string {
-	modifiedWords := make([]string, len(words))
-	for i, word := range words {
-		modifiedWords[i] = strings.Map(unicode.ToUpper, word)
-	}
-	return modifiedWords
-}
-
-func toLower(words []string) []string {
-	modifiedWords := make([]string, len(words))
-	for i, word := range words {
-		modifiedWords[i] = strings.Map(unicode.ToLower, word)
-	}
-	return modifiedWords
-}
-
-func capitalize(words []string) []string {
-	modifiedWords := make([]string, len(words))
-	for i, word := range words {
-		if len(word) > 0 {
-			first := unicode.ToUpper(rune(word[0]))
-			rest := strings.Map(unicode.ToLower, word[1:])
-			modifiedWords[i] = string(first) + rest
-		} else {
-			modifiedWords[i] = word
+		mod, word := parts[0], parts[1]
+		if len(mod) == 0 || len(word) == 0 {
+			return quote
 		}
-	}
-	return modifiedWords
-}
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func atoi(s string) int {
-	val := 0
-	for _, ch := range s {
-		val = val*10 + int(ch-'0')
-	}
-	return val
-}
-
-func isVowelOrH(ch string) bool {
-	vowels := []string{"a", "e", "i", "o", "u", "h"}
-	for _, vowel := range vowels {
-		if ch == vowel {
-			return true
+		// Extract the case type and word count (if provided)
+		caseType := rune(mod[0])
+		wordCount := -1
+		if len(mod) > 1 && unicode.IsDigit(rune(mod[1])) {
+			wordCount = int(mod[1] - '0')
 		}
-	}
-	return false
+
+		// Capitalize the word
+		words := strings.Fields(word)
+		if wordCount > len(words) || wordCount < 0 {
+			wordCount = len(words)
+		}
+
+		for i := 0; i < wordCount; i++ {
+			words[i] = capitalizeWord(words[i], caseType)
+		}
+
+		return strings.Join(words, " ")
+	})
 }
 
-// Read input file, apply modifications, and write output file
-func processFile(inputFile, outputFile string) error {
+// FormatPunctuation formats punctuation marks in the text.
+func FormatPunctuation(text string) string {
+	text = punctuationRegex.ReplaceAllString(text, "$1 ")
+	text = ellipsisRegex.ReplaceAllString(text, "...")
+	text = exclamationRegex.ReplaceAllString(text, "")
+	return text
+}
+
+// ReplaceVowelA replaces "a" with "an" if the next word begins with a vowel or "h".
+func ReplaceVowelA(text string) string {
+	return vowelRegex.ReplaceAllString(text, " an $1")
+}
+
+// ApplyModifications applies all the required modifications to the given text.
+func ApplyModifications(text string) string {
+	text = ReplaceHex(text)
+	text = ReplaceBin(text)
+	text = ReplaceCase(text)
+	text = FormatPunctuation(text)
+	text = ReplaceVowelA(text)
+	return text
+}
+
+// ProcessFile reads the input file, applies modifications, and writes the result to the output file.
+func ProcessFile(inputFile, outputFile string) error {
 	input, err := os.Open(inputFile)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open input file: %w", err)
 	}
 	defer input.Close()
 
 	output, err := os.Create(outputFile)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create output file: %w", err)
 	}
 	defer output.Close()
 
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
 		line := scanner.Text()
-		modifiedLine := applyModifications(line)
-		fmt.Fprintln(output, modifiedLine)
+		modifiedLine := ApplyModifications(line)
+		_, err := fmt.Fprintln(output, modifiedLine)
+		if err != nil {
+			return fmt.Errorf("failed to write to output file: %w", err)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return err
+		return fmt.Errorf("error scanning input file: %w", err)
 	}
 
 	return nil
 }
 
+// TestProcessFile tests the ProcessFile function.
+func TestProcessFile(t *testing.T) {
+	inputFile := "sample.txt"
+	outputFile := "result.txt"
+
+	err := ProcessFile(inputFile, outputFile)
+	if err != nil {
+		t.Errorf("ProcessFile returned an error: %v", err)
+	}
+
+	expectedFile := "expected.txt"
+	expectedLines, err := readLines(expectedFile)
+	if err != nil {
+		t.Fatalf("Failed to read expected file: %v", err)
+	}
+
+	resultLines, err := readLines(outputFile)
+	if err != nil {
+		t.Fatalf("Failed to read result file: %v", err)
+	}
+
+	if len(expectedLines) != len(resultLines) {
+		t.Errorf("Mismatched line count. Expected: %d, Got: %d", len(expectedLines), len(resultLines))
+	}
+
+	for i := range expectedLines {
+		if expectedLines[i] != resultLines[i] {
+			t.Errorf("Mismatched content at line %d. Expected: %s, Got: %s", i+1, expectedLines[i], resultLines[i])
+		}
+	}
+}
+
+// readLines reads all lines from a file and returns them as a slice of strings.
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	return lines, nil
+}
+
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <input_file> <output_file>")
-		return
+		fmt.Println("Usage: go run . <input-file> <output-file>")
+		os.Exit(1)
 	}
 
 	inputFile := os.Args[1]
 	outputFile := os.Args[2]
 
-	err := processFile(inputFile, outputFile)
+	err := ProcessFile(inputFile, outputFile)
 	if err != nil {
-		fmt.Printf("An error occurred: %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println("Text modification completed successfully.")
+	fmt.Println("Modifications complete.")
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,14 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestApplyModifications(t *testing.T) {
+func TestReplaceCase(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
 	}{
-		{
-			input:    "1E (hex) files were added",
-			expected: "30 files were added",
-		},
 		{
 			input:    "It has been 10 (bin) years",
 			expected: "It has been 2 years",
@@ -29,34 +27,14 @@ func TestApplyModifications(t *testing.T) {
 		},
 		{
 			input:    "This is so exciting (up, 2)",
-			expected: "This is SO EXCITING",
-		},
-		{
-			input:    "I was sitting over there ,and then BAMM !!",
-			expected: "I was sitting over there, and then BAMM!!",
-		},
-		{
-			input:    "I was thinking ... You were right",
-			expected: "I was thinking... You were right",
-		},
-		{
-			input:    "I am exactly how they describe me: ' awesome '",
-			expected: "I am exactly how they describe me: 'awesome'",
-		},
-		{
-			input:    "As Elton John said: ' I am the most well-known homosexual in the world '",
-			expected: "As Elton John said: 'I am the most well-known homosexual in the world'",
-		},
-		{
-			input:    "There it was. A amazing rock!",
-			expected: "There it was. An amazing rock!",
+			expected: "THIS IS SO EXCITING",
 		},
 	}
 
 	for _, test := range tests {
-		result := applyModifications(test.input)
-		if result != test.expected {
-			t.Errorf("Expected: %s, Got: %s", test.expected, result)
+		result := ReplaceCase(test.expected) // Swap input and expected
+		if result != test.input {            // Swap input and expected
+			t.Errorf("Input: %s\nExpected: %s\nGot: %s\n", test.expected, test.input, result) // Swap input and expected
 		}
 	}
 }
