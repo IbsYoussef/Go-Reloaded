7899c93b3b033cf50ec54af856f744eadbda5529
iyoussef <ibraheemyoussefiy@gmail.com>
2024-09-10 07:37:28 +0100

    ConvertBinToDecimal function implemented (passed all unit tests)

---
+++ internal/textmod/mod_bin_test.go
@@ -0,0 +1,21 @@
+package textmod
+
+import "testing"
+
+func TestConvertBinToDecimal(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"101 (bin) is a small number", "5 is a small number"},
+		{"I paid 1001 (bin) dollars", "I paid 9 dollars"},
+		{"110 (bin), 111 (bin), and 1000 (bin) are all binary numbers", "6, 7, and 8 are all binary numbers"},
+	}
+
+	for _, test := range tests {
+		result := ConvertBinToDecimal(test.input)
+		if result != test.expected {
+			t.Errorf("For input %s, expected: %s, but got: %s", test.input, test.expected, result)
+		}
+	}
+}
