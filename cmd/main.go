de65bf9d22431c440e5f6dc91f2af1649b74e5bc
iyoussef <ibraheemyoussefiy@gmail.com>
2025-02-05 16:04:21 +0000

    Minor changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,5 @@
 package main
 
 func main() {
-	// Every instance of punctuations . , ! ? : ; should be moved next to the previous word, with a space apart from the next one
-	testString := "I was sitting over there ,and then BAMM !!"
-
-	// Groups of punctuations like ... or !? should be formatted like so
-	// "I was thinking... You were right"
-	testString2 := "I was thinking ... You were right"
+	
 }
