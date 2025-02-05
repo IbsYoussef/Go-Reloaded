9917a87f46e8d9713058bd96998668fa4b1cc60d
iyoussef <ibraheemyoussefiy@gmail.com>
2025-02-05 14:31:33 +0000

    Final implementation of ChangeCase function

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go-reloaded/internal/textmod"
+)
 
+func main() {
+	test := "one (up)"
+	test2 := "one, two, three, four (up, 4)"
+	test3 := "ONE TWO THREE FOUR (low, 4)"
+	test4 := "one two three four (cap, 4)"
+	test5 := "TWO (low)"
+	test6 := "three (cap)"
+	fmt.Println(textmod.ChangeCaseFinal(test))
+	fmt.Println(textmod.ChangeCaseFinal(test2))
+	fmt.Println(textmod.ChangeCaseFinal(test3))
+	fmt.Println(textmod.ChangeCaseFinal(test4))
+	fmt.Println(textmod.ChangeCaseFinal(test5))
+	fmt.Println(textmod.ChangeCaseFinal(test6))
 }
--- internal/textmod/case.go
+++ internal/textmod/case.go
@@ -1,6 +1,7 @@
 package textmod
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -10,6 +11,10 @@ func ChangeCase(text string) string {
 	}
 
 	strSlice := strings.Fields(text)
+
+	if strSlice[0] == "(cap)" || strSlice[0] == "(low)" || strSlice[0] == "(up)" {
+		return "Invalid tag format"
+	}
 
 	for i := 0; i < len(strSlice); i++ {
 		if strSlice[i] == "(up)" || strSlice[i] == "(low)" || strSlice[i] == "(cap)" {
@@ -22,7 +27,7 @@ func ChangeCase(text string) string {
 				case "(low)":
 					word = strings.ToLower(word)
 				case "(cap)":
-					word = strings.Title(word)
+					word = strings.ToTitle(word)
 				}
 				strSlice[i-1] = word
 				strSlice = append(strSlice[:i], strSlice[i+1:]...)
@@ -33,5 +38,139 @@ func ChangeCase(text string) string {
 		}
 	}
 
+	return strings.Join(strSlice, " ")
+}
+
+func ChangeCaseNnew(text string) string {
+	if text == "" {
+		return text
+	}
+
+	strSlice := strings.Fields(text)
+
+	switch strSlice[0] {
+	case "(cap,", "(low,", "(up,", "(cap)", "(low)", "(up)":
+		return "Tag cannot be at the start"
+	}
+
+	for i := 0; i < len(strSlice); i++ {
+		if strSlice[i] == "(cap," || strSlice[i] == "(low," || strSlice[i] == "(up," {
+			tagNum := strSlice[i+1]
+			numStr := ""
+			if strings.HasSuffix(tagNum, ")") {
+				numStr = strings.TrimSuffix(tagNum, ")")
+			}
+			if i > 0 {
+				switch strSlice[i] {
+				case "(cap,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.Title(strSlice[i-j])
+					}
+				case "(low,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.ToLower(strSlice[i-j])
+					}
+
+				case "(up,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.ToUpper(strSlice[i-j])
+					}
+				}
+			}
+			strSlice[i], strSlice[i+1] = "", ""
+		}
+	}
+
+	return strings.Join(strSlice, " ")
+}
+
+func ChangeCaseFinal(text string) string {
+	if text == "" {
+		return text
+	}
+
+	strSlice := strings.Fields(text)
+
+	switch strSlice[0] {
+	case "(cap,", "(low,", "(up,", "(cap)", "(low)", "(up)":
+		return "Tag cannot be at the start"
+	}
+
+	for i := 0; i < len(strSlice); i++ {
+		if strSlice[i] == "(up)" || strSlice[i] == "(low)" || strSlice[i] == "(cap)" {
+			tag := strSlice[i]
+			if i > 0 {
+				word := strSlice[i-1]
+				switch tag {
+				case "(cap)":
+					word = strings.Title(word)
+				case "(low)":
+					word = strings.ToLower(word)
+				case "(up)":
+					word = strings.ToUpper(word)
+				}
+				strSlice[i-1] = word
+				strSlice = append(strSlice[:i], strSlice[i+1:]...)
+				i--
+			}
+		}
+
+		if strSlice[i] == "(cap," || strSlice[i] == "(low," || strSlice[i] == "(up," {
+			tagNum := strSlice[i+1]
+			numStr := ""
+			if strings.HasSuffix(tagNum, ")") {
+				numStr = strings.TrimSuffix(tagNum, ")")
+			}
+
+			if i > 0 {
+				switch strSlice[i] {
+				case "(cap,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.Title(strSlice[i-j])
+					}
+				case "(low,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.ToLower(strSlice[i-j])
+					}
+
+				case "(up,":
+					number, err := strconv.Atoi(numStr)
+					if err != nil {
+						return "Could not convert string number to integer"
+					}
+
+					for j := 1; j <= number; j++ {
+						strSlice[i-j] = strings.ToUpper(strSlice[i-j])
+					}
+				}
+				strSlice[i], strSlice[i+1] = "", ""
+			}
+		}
+	}
 	return strings.Join(strSlice, " ")
 }
