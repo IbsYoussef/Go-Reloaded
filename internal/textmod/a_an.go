70b62f61e08c46810dd4c216694182f5bbbc762d
iyoussef <ibraheemyoussefiy@gmail.com>
2025-02-07 15:38:00 +0000

    A_an function implemented

--- internal/textmod/a_an.go
+++ internal/textmod/a_an.go
@@ -1,5 +1,18 @@
 package textmod
 
-// func ConvertAtoAn(text string) string {
+import "strings"
 
-// }
+func ConvertAtoAn(text string) string {
+	words := strings.Fields(text)
+
+	for i := 0; i < len(words)-1; i++ {
+		if words[i] == "a" || words[i] == "A" {
+			if len(words[i+1]) > 0 && strings.Contains("aeiouAEIOU", string(words[i+1][0])) {
+				words[i] = words[i] + "n"
+			}
+		}
+	}
+
+	text = strings.Join(words, " ")
+	return text
+}
