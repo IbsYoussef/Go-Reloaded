46438a8f6030b6f93086d9cc46afb674f0dddbde
iyoussef <ibraheemyoussefiy@gmail.com>
2025-02-10 17:10:46 +0000

    Punctuation function implemented

--- internal/textmod/punctuation.go
+++ internal/textmod/punctuation.go
@@ -1,5 +1,17 @@
 package textmod
 
-// func ModifyPunctuation(text string) string {
+import (
+	"regexp"
+	"strings"
+)
 
-// }
+func ModifyPunctuationAndQuotes(text string) string {
+	re := regexp.MustCompile(`\s*([.,!;:?]+)\s*`)
+	text = re.ReplaceAllString(text, "$1 ")
+
+	reStartQuote := regexp.MustCompile(`' `)
+	text = reStartQuote.ReplaceAllString(text, " '")
+
+	reEndQuote := regexp.MustCompile(` '`)
+	return strings.TrimSpace(reEndQuote.ReplaceAllString(text, "'"))
+}
