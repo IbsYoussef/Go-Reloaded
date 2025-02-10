31e90d0c855516ae7c3659c81bf13e25c1a0fc3c
iyoussef <ibraheemyoussefiy@gmail.com>
2025-02-10 17:17:40 +0000

    Implemented Modify text function

--- internal/textmod/textmod.go
+++ internal/textmod/textmod.go
@@ -1,5 +1,10 @@
 package textmod
 
-// func ModifyText(text string) string {
-
-// }
+func ModifyText(text string) string {
+	text = ConvertBinToDecimal(text)
+	text = ConvertHexToDecimal(text)
+	text = ConvertAtoAn(text)
+	text = ChangeCase(text)
+	text = ModifyPunctuationAndQuotes(text)
+	return text
+}
